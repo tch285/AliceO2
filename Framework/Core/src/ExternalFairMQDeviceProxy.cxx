096694b4a397ca5505cc867bd6db79252b2e1c38
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2024-11-07 06:54:40 +0100

    DPL: allow larger grace period for dispatching non-DPL incoming messages (#13639)

--- Framework/Core/src/ExternalFairMQDeviceProxy.cxx
+++ Framework/Core/src/ExternalFairMQDeviceProxy.cxx
@@ -108,8 +108,8 @@ void sendOnChannel(fair::mq::Device& device, fair::mq::Parts& messages, std::str
   }
 
   // FIXME: we need a better logic for avoiding message spam
-  if (timeout > 1 && timeout <= maxTimeout) {
-    LOG(warning) << "dispatching on channel " << channel << " was delayed by " << timeout << " ms";
+  if (timeout > 100 && timeout <= maxTimeout) {
+    LOG(warning) << "dispatching on channel " << channel << " was delayed by " << timeout / 1000.f << " s";
   }
   // TODO: feeling this is a bit awkward, but the interface of fair::mq::Parts does not provide a
   // method to clear the content.
