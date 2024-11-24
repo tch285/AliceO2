83049afbb3b006fa19be960363f66b53485a3bec
shahoian <ruben.shahoyan@cern.ch>
2024-11-24 18:42:16 +0100

    Add AltroSyncSignal to TPC CDBTypeMap

--- Detectors/TPC/base/include/TPCBase/CDBTypes.h
+++ Detectors/TPC/base/include/TPCBase/CDBTypes.h
@@ -24,68 +24,70 @@ namespace o2::tpc
 
 /// Calibration and parameter types for CCDB
 enum class CDBType {
-  CalPedestal,         ///< Pedestal calibration
-  CalNoise,            ///< Noise calibration
-  CalPedestalNoise,    ///< Pedestal and Noise calibration
-  CalPulser,           ///< Pulser calibration
-  CalCE,               ///< Laser CE calibration
-  CalPadGainFull,      ///< Full pad gain calibration
-  CalPadGainResidual,  ///< ResidualpPad gain calibration (e.g. from tracks)
-  CalLaserTracks,      ///< Laser track calibration data
-  CalVDriftTgl,        ///< ITS-TPC difTgl vdrift calibration
-  CalTimeGain,         ///< Gain variation over time
-  CalTimeGainMC,       ///< Gain variation over time for MC
-  CalGas,              ///< DCS gas measurements
-  CalTemperature,      ///< DCS temperature measurements
-  CalHV,               ///< DCS HV measurements
-  CalTopologyGain,     ///< Q cluster topology correction
-                       ///
-  ConfigFEEPad,        ///< FEE pad-by-pad configuration map
-  ConfigFEE,           ///< FEE configuration map for each tag
-  ConfigRunInfo,       ///< FEE run information (run -> tag)
-                       ///
-  ParDetector,         ///< Parameter for Detector
-  ParElectronics,      ///< Parameter for Electronics
-  ParGas,              ///< Parameter for Gas
-  ParGEM,              ///< Parameter for GEM
-                       ///
-  CalIDC0A,            ///< I_0(r,\phi) = <I(r,\phi,t)>_t
-  CalIDC0C,            ///< I_0(r,\phi) = <I(r,\phi,t)>_t
-  CalIDC1A,            ///< I_1(t) = <I(r,\phi,t) / I_0(r,\phi)>_{r,\phi}
-  CalIDC1C,            ///< I_1(t) = <I(r,\phi,t) / I_0(r,\phi)>_{r,\phi}
-  CalIDCDeltaA,        ///< \Delta I(r,\phi,t) = I(r,\phi,t) / ( I_0(r,\phi) * I_1(t) )
-  CalIDCDeltaC,        ///< \Delta I(r,\phi,t) = I(r,\phi,t) / ( I_0(r,\phi) * I_1(t) )
-  CalIDCFourierA,      ///< Fourier coefficients of CalIDC1
-  CalIDCFourierC,      ///< Fourier coefficients of CalIDC1
-  CalIDCPadStatusMapA, ///< Status map of the pads (dead etc. obatined from CalIDC0)
-  CalIDCPadStatusMapC, ///< Status map of the pads (dead etc. obatined from CalIDC0)
-  CalIDCGroupingParA,  ///< Parameters which were used for the averaging of the CalIDCDelta
-  CalIDCGroupingParC,  ///< Parameters which were used for the averaging of the CalIDCDelta
-                       ///
-  CalSAC0,             ///< I_0(r,\phi) = <I(r,\phi,t)>_t
-  CalSAC1,             ///< I_1(t) = <I(r,\phi,t) / I_0(r,\phi)>_{r,\phi}
-  CalSACDelta,         ///< \Delta I(r,\phi,t) = I(r,\phi,t) / ( I_0(r,\phi) * I_1(t) )
-  CalSACFourier,       ///< Fourier coefficients of CalSAC1
-                       ///
-  CalITPC0,            ///< 2D average TPC clusters for longer time interval
-  CalITPC1,            ///< 1D integrated TPC clusters
-                       ///
-  CalCorrMap,          ///< Cluster correction map (high IR rate distortions)
-  CalCorrMapRef,       ///< Cluster correction reference map (static distortions)
-  CalCorrMapMC,        ///< Cluster correction map (high IR rate distortions) for MC
-  CalCorrDerivMapMC,   ///< Cluster correction reference map (static distortions) for MC
-                       ///
-  CalCorrDerivMap,     ///< Cluster correction map (derivative map)
-                       ///
-  CalTimeSeries,       ///< integrated DCAs for longer time interval
-  CalScaler,           ///< Scaler from IDCs or combined estimator
-  CalScalerWeights,    ///< Weights for scalers
-  CalMShape,           ///< calibration object for M-shape distortions
-                       ///
-  CorrMapParam,        ///< parameters for CorrectionMapsLoader configuration
-                       ///
-  DistortionMapMC,     ///< full distortions (static + IR dependant) for MC used in the digitizer
-  DistortionMapDerivMC ///< derivative distortions for MC used in the digitizer for scaling
+  CalPedestal,          ///< Pedestal calibration
+  CalNoise,             ///< Noise calibration
+  CalPedestalNoise,     ///< Pedestal and Noise calibration
+  CalPulser,            ///< Pulser calibration
+  CalCE,                ///< Laser CE calibration
+  CalPadGainFull,       ///< Full pad gain calibration
+  CalPadGainResidual,   ///< ResidualpPad gain calibration (e.g. from tracks)
+  CalLaserTracks,       ///< Laser track calibration data
+  CalVDriftTgl,         ///< ITS-TPC difTgl vdrift calibration
+  CalTimeGain,          ///< Gain variation over time
+  CalTimeGainMC,        ///< Gain variation over time for MC
+  CalGas,               ///< DCS gas measurements
+  CalTemperature,       ///< DCS temperature measurements
+  CalHV,                ///< DCS HV measurements
+  CalTopologyGain,      ///< Q cluster topology correction
+                        ///
+  ConfigFEEPad,         ///< FEE pad-by-pad configuration map
+  ConfigFEE,            ///< FEE configuration map for each tag
+  ConfigRunInfo,        ///< FEE run information (run -> tag)
+                        ///
+  ParDetector,          ///< Parameter for Detector
+  ParElectronics,       ///< Parameter for Electronics
+  ParGas,               ///< Parameter for Gas
+  ParGEM,               ///< Parameter for GEM
+                        ///
+  CalIDC0A,             ///< I_0(r,\phi) = <I(r,\phi,t)>_t
+  CalIDC0C,             ///< I_0(r,\phi) = <I(r,\phi,t)>_t
+  CalIDC1A,             ///< I_1(t) = <I(r,\phi,t) / I_0(r,\phi)>_{r,\phi}
+  CalIDC1C,             ///< I_1(t) = <I(r,\phi,t) / I_0(r,\phi)>_{r,\phi}
+  CalIDCDeltaA,         ///< \Delta I(r,\phi,t) = I(r,\phi,t) / ( I_0(r,\phi) * I_1(t) )
+  CalIDCDeltaC,         ///< \Delta I(r,\phi,t) = I(r,\phi,t) / ( I_0(r,\phi) * I_1(t) )
+  CalIDCFourierA,       ///< Fourier coefficients of CalIDC1
+  CalIDCFourierC,       ///< Fourier coefficients of CalIDC1
+  CalIDCPadStatusMapA,  ///< Status map of the pads (dead etc. obatined from CalIDC0)
+  CalIDCPadStatusMapC,  ///< Status map of the pads (dead etc. obatined from CalIDC0)
+  CalIDCGroupingParA,   ///< Parameters which were used for the averaging of the CalIDCDelta
+  CalIDCGroupingParC,   ///< Parameters which were used for the averaging of the CalIDCDelta
+                        ///
+  CalSAC0,              ///< I_0(r,\phi) = <I(r,\phi,t)>_t
+  CalSAC1,              ///< I_1(t) = <I(r,\phi,t) / I_0(r,\phi)>_{r,\phi}
+  CalSACDelta,          ///< \Delta I(r,\phi,t) = I(r,\phi,t) / ( I_0(r,\phi) * I_1(t) )
+  CalSACFourier,        ///< Fourier coefficients of CalSAC1
+                        ///
+  CalITPC0,             ///< 2D average TPC clusters for longer time interval
+  CalITPC1,             ///< 1D integrated TPC clusters
+                        ///
+  CalCorrMap,           ///< Cluster correction map (high IR rate distortions)
+  CalCorrMapRef,        ///< Cluster correction reference map (static distortions)
+  CalCorrMapMC,         ///< Cluster correction map (high IR rate distortions) for MC
+  CalCorrDerivMapMC,    ///< Cluster correction reference map (static distortions) for MC
+                        ///
+  CalCorrDerivMap,      ///< Cluster correction map (derivative map)
+                        ///
+  CalTimeSeries,        ///< integrated DCAs for longer time interval
+  CalScaler,            ///< Scaler from IDCs or combined estimator
+  CalScalerWeights,     ///< Weights for scalers
+  CalMShape,            ///< calibration object for M-shape distortions
+                        ///
+  CorrMapParam,         ///< parameters for CorrectionMapsLoader configuration
+                        ///
+  DistortionMapMC,      ///< full distortions (static + IR dependant) for MC used in the digitizer
+  DistortionMapDerivMC, ///< derivative distortions for MC used in the digitizer for scaling
+
+  AltroSyncSignal ///< timing of Altro chip sync. signal
 };
 
 /// Storage name in CCDB for each calibration and parameter type
@@ -153,6 +155,8 @@ const std::unordered_map<CDBType, const std::string> CDBTypeMap{
   // distortion maps
   {CDBType::DistortionMapMC, "TPC/Calib/DistortionMapMC"},
   {CDBType::DistortionMapDerivMC, "TPC/Calib/DistortionMapDerivativeMC"},
+  // AltroSyncSignal
+  {CDBType::AltroSyncSignal, "TPC/Config/AltroSyncSignal"},
 };
 
 } // namespace o2::tpc
